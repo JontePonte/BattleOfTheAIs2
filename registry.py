ba23f6f52ebc3fd7c43f59c0627e7267d6acf875
JontePonte <john_nordstrand@hotmail.com>
2024-11-23 20:41:53 +0100

    added registry file

---
+++ registry.py
@@ -0,0 +1,11 @@
+""" Add new AI:s here """
+
+from ai_contenders.full_random import full_random
+from ai_contenders.random_attacker import random_attacker
+
+# All condenders are added here
+
+ai_list = [
+    full_random,
+    random_attacker
+]
